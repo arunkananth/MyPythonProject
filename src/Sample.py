a43dbcc554d2dcda179326b607b755c00ece16ce
arunkananth <arunkananth@gmail.com>
2017-08-24 10:49:11 -0500

    Add Sample.py to repository

---
+++ src/Sample.py
@@ -0,0 +1,23 @@
+'''
+Created on Jun 22, 2017
+
+@author: hdusr
+'''
+import pandas as pd
+import numpy as np
+import matplotlib
+#import matplotlib.pyplot as mp
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+ts = ts.cumsum()
+
+df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index,
+                  columns=['A', 'B', 'C', 'D'])
+df = df.cumsum()
+#df.plot();
+plt.ion()
+plt.plot(df)
+#plt.legend(loc='best')
