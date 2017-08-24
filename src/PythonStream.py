f8702bce5be1879ce7644e2f9d569a4e64f8c0b8
arunkananth <arunkananth@gmail.com>
2017-08-24 10:07:38 -0500

    Sample programs - to github - initial commit

---
+++ src/PythonStream.py
@@ -0,0 +1,41 @@
+'''
+Created on Aug 2, 2017
+
+@author: hdusr
+'''
+#!/usr/bin/env python2
+
+
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "1528192604-pWoRvXqulgbS1LBwT66qYvsy8L5zdOomapCHO4g"
+access_token_secret = "vlmN10qPajHwJViSuPmcDZUHVtwjalm7QPmtzJnZN8zAG"
+consumer_key = "OB48XMh4S1DEVJ4tTsphVg9px"
+consumer_secret = "qwivWEKmG22jAeXn7VxUr6ETxQOwuRG4KKZLk00DQFeYbWCfhv"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['India', 'China', 'America'])
---
+++ src/mysql_connection_test.py
@@ -0,0 +1,32 @@
+'''
+Created on Aug 1, 2017
+
+@author: hdusr
+'''
+import mysql.connector
+import pandas as pd
+
+db = mysql.connector.connect(user='hdusr', password='mercury*88',
+                              host='localhost',
+                              database='MyDB')
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SELECT min(company_id),company_nm FROM company group by company_nm")
+
+# print all the first cell of all the rows
+for row in cur.fetchall():
+    print row[0]
+
+
+df_mysql = pd.read_sql('select * from company;', con=db)    
+print ('loaded dataframe from MySQL. records:', len(df_mysql))
+
+    
+
+for (company_id, company_nm) in cur:
+    print("{} , {}".format(company_id, company_nm))
+
+db.close()
