ada0ec1089bbc100682ad1cec603b9b5249031d9
Arun Ananthakrishnan <arunkananth@gmail.com>
2018-03-18 01:36:24 -0400

    Delete config.py

--- src/config.py
+++
@@ -1,5 +0,0 @@
-#Variables that contains the user credentials to access Twitter API 
-access_token = "1528192604-pWoRvXqulgbS1LBwT66qYvsy8L5zdOomapCHO4g"
-access_token_secret = "vlmN10qPajHwJViSuPmcDZUHVtwjalm7QPmtzJnZN8zAG"
-consumer_key = "OB48XMh4S1DEVJ4tTsphVg9px"
-consumer_secret = "qwivWEKmG22jAeXn7VxUr6ETxQOwuRG4KKZLk00DQFeYbWCfhv"
